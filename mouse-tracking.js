3ce4beb7996856895468272137b0d4336fabf65f
hogeleka <hogeleka@macalester.edu>
2018-12-10 16:54:57 -0600

    some updates from class with Paul

--- mouse-tracking.js
+++ mouse-tracking.js
@@ -11,7 +11,59 @@ var TrackMouse = function (mouseEvent) {
     console.log( "X: " + mouseEvent.x + ", Y: " + mouseEvent.y + "\n");
 };
 
-document.addEventListener('click', TrackMouse);
+// document.addEventListener = () => {
+//     console.log("Custom document event occured");
+// };
+
+window.addEventListener = function() {
+    if (arguments.length > 1) {
+        if (arguments[0] === 'click') {
+            console.log("Custom window event occurred");
+        }
+    }
+};
+
+// game.canvas.addEventListener = () => {
+//     if (arguments.length > 1) {
+//         if (arguments[0] === 'click') {
+//             console.log("Custom game event occurred");
+//         }
+//     }
+// };
+
+// Phaser.MSPointer.prototype.game.canvas.addEventListener = () => {
+//     if (arguments.length >= 2) {
+//         if (arguments[0] === 'click') {
+//             console.log("Custom canvas event occured");
+//         }
+//     }
+// };
+// Phaser.Mouse.prototype.game.canvas.addEventListener = () => {
+//     if (arguments.length >= 2) {
+//         if (arguments[0] === 'click') {
+//             console.log("Custom canvas event occured");
+//         }
+//     }
+// };
+
+//document.getElementById("gameContainer").__proto__.__proto__.__proto__.__proto__.__proto__.addEventListener = () => console.log("YARRRR");
+
+let originalAddEventListener = EventTarget.prototype.addEventListener;
+EventTarget.prototype.addEventListener = (event, handler, option) => {
+    let wrappedHandler = (a, b, c) => {
+        console.log("got event", a, b, c);
+        handler(a, b, c);
+    };
+    originalAddEventListener(event, wrappedHandler, option);
+};
+// this.game.canvas.addEventListener = function () {
+//     if (arguments.length >= 2) {
+//         if (arguments[0] === 'click') {
+//             console.log("Custom canvas event occured");
+//         }
+//     }
+// };
+
 // document.addEventListener('mousemove', TrackMouse);
 // document.addEventListener('mousedown', TrackMouse);
 // document.addEventListener('mouseup', TrackMouse);
