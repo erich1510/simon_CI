88e58d0dfac01188bf06645df1e96caef3ec9d5a
RohitBagda <rohitbagda@gmail.com>
2018-12-14 17:19:08 -0600

    Work Done in Class

--- mouse-tracking.js
+++ mouse-tracking.js
@@ -1,69 +1,45 @@
-var eventCount = 0;
-var eventProperty = [];
-
-var TrackMouse = function (mouseEvent) {
-    eventProperty[eventCount++] = {
-        type: 'mouse',
-        ts: Date.now(),
-        x: mouseEvent.x,
-        y: mouseEvent.y
-    };
-    console.log( "X: " + mouseEvent.x + ", Y: " + mouseEvent.y + "\n");
-};
-
-// document.addEventListener = () => {
-//     console.log("Custom document event occured");
-// };
-
-window.addEventListener = function() {
-    if (arguments.length > 1) {
-        if (arguments[0] === 'click') {
-            console.log("Custom window event occurred");
-        }
-    }
-};
-
-// game.canvas.addEventListener = () => {
-//     if (arguments.length > 1) {
-//         if (arguments[0] === 'click') {
-//             console.log("Custom game event occurred");
-//         }
-//     }
-// };
-
-// Phaser.MSPointer.prototype.game.canvas.addEventListener = () => {
-//     if (arguments.length >= 2) {
-//         if (arguments[0] === 'click') {
-//             console.log("Custom canvas event occured");
-//         }
-//     }
-// };
-// Phaser.Mouse.prototype.game.canvas.addEventListener = () => {
-//     if (arguments.length >= 2) {
-//         if (arguments[0] === 'click') {
-//             console.log("Custom canvas event occured");
-//         }
-//     }
-// };
-
 //document.getElementById("gameContainer").__proto__.__proto__.__proto__.__proto__.__proto__.addEventListener = () => console.log("YARRRR");
+
+var fs = require('fs');
+
+function loggingProxy(target) {
+    let handler = {
+        get: function(obj, prop) {
+            if(!(prop in obj)) {
+                console.log("Tried to get nonexistent property", prop, "on", obj);
+            }
+            return loggingProxy(obj[prop]);
+        }
+    };
+    return new Proxy(target, handler);
+};
+
 
 let originalAddEventListener = EventTarget.prototype.addEventListener;
 EventTarget.prototype.addEventListener = (event, handler, option) => {
-    let wrappedHandler = (a, b, c) => {
-        console.log("got event", a, b, c);
-        handler(a, b, c);
+    let wrappedHandler = (eventCallback, options) => {
+        if(event === 'click'){
+            ///here we send the event info to separate file
+            console.log(eventCallback.clientX, ':', eventCallback.clientY);
+            eventCallback.clientX = 500;
+            console.log(eventCallback.clientX, ':', eventCallback.clientY);
+            console.log("got event", event, eventCallback);
+            // fs.writeFile("data.json", jsonData, function(err) {
+            //     if (err) {
+            //         console.log(err);
+            //     }
+            // });
+
+        }
+        handler(eventCallback);
     };
     originalAddEventListener(event, wrappedHandler, option);
-};
-// this.game.canvas.addEventListener = function () {
-//     if (arguments.length >= 2) {
-//         if (arguments[0] === 'click') {
-//             console.log("Custom canvas event occured");
-//         }
-//     }
-// };
 
-// document.addEventListener('mousemove', TrackMouse);
-// document.addEventListener('mousedown', TrackMouse);
-// document.addEventListener('mouseup', TrackMouse);
+
+console.log("-------> 0");
+    setTimeout(() => {
+        // read and send a fake event that came from playback file
+        handler(loggingProxy({}));
+    }, 200);
+};
+
