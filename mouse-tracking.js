a266b3769bed19824a09de38f8581968c3e8306f
RohitBagda <rohitbagda@gmail.com>
2018-12-19 23:19:06 -0600

    Added Console Logs

--- mouse-tracking.js
+++ mouse-tracking.js
@@ -1,82 +1,117 @@
-//document.getElementById("gameContainer").__proto__.__proto__.__proto__.__proto__.__proto__.addEventListener = () => console.log("YARRRR");
 
-// var fs = require('fs');
+/**
+ * Recording Phase
+ */
 
-// function loggingProxy(target) {
-//     let handler = {
-//         get: function(obj, prop) {
-//             if(!(prop in obj)) {
-//                 console.log("Tried to get nonexistent property", prop, "on", obj);
-//             }
-//             return loggingProxy(obj[prop]);
-//         }
-//     };
-//     return new Proxy(target, handler);
-// };
-
-var gameContainer = document.getElementById("gameContainer");
-
+//Store original EventListener
 let originalAddEventListener = EventTarget.prototype.addEventListener;
+let clickCounter = 0;
+let clickEventCounter = 0;
+
+//Monkey-patch EventListener.
 EventTarget.prototype.addEventListener = (event, handler, option) => {
-    let wrappedHandler = (eventCallback, options) => {
-        console.log("got event", event, " at ", eventCallback.clientX, eventCallback.clientY, "with properties: ", eventCallback);
 
-        if(event === 'click'){
-            ///here we send the event info to separate file
-            // console.log(eventCallback.clientX, ':', eventCallback.clientY);
-            // eventCallback.clientX = 500;
-            // console.log(eventCallback.clientX, ':', eventCallback.clientY);
-            // console.log("got event", event, " at ", eventCallback.clientX, eventCallback.clientY, "with properties: ", eventCallback);
-            // fs.writeFile("data.json", jsonData, function(err) {
-            //     if (err) {
-            //         console.log(err);
-            //     }
-            // });
+    //The wrapped Event Handler logs the coordinates of the mouse clicks and properties of the Pointer Events and
+    // calls the original handler.
+    let wrappedHandler = (eventCallback) => {
 
+        //This check avoids logging of initial double click required to start the game.
+        if(clickCounter>=2){
+
+            //This check helps demarcate each chunk of pointer events for better readability.
+            if(event==='click'){
+                console.log(" Recorded event ", event, " at X:", eventCallback.clientX, " Y: ", eventCallback.clientY , "\n", " ------Click Event Complete----" );
+            } else {
+                console.log(" Recorded event ", event, " at X:", eventCallback.clientX, " Y: ", eventCallback.clientY);
+            }
         }
+
+        //Since each mouse click constitutes of 3 click events we want to avoid counting
+        // multiple click events for each actual mouse click.
+        if(event==='click'){
+            clickEventCounter++;
+            if(clickEventCounter%3 === 0){
+                clickCounter++;
+            }
+        }
+
+        //Call Original callback handler after data is logged.
         handler(eventCallback);
     };
+
+    //Add the original event listener with the wrapped callback.
     originalAddEventListener(event, wrappedHandler, option);
-
-
-console.log("-------> 0");
-    // setTimeout(() => {
-    //     // read and send a fake event that came from playback file
-    //     handler(loggingProxy({}));
-    // }, 200);
 };
 
-var clickCounter = 0;
 
-doClickEvent = function() {
+
+//
+// var plays = []; // This array will hold the different indices of the boxes selected by the computer.
+//
+// //Monkey-Patched Phaser Random Integer Generator.
+// Phaser.RandomDataGenerator.prototype.integerInRange = (min, max) => {
+//
+//     //Generate our own random number.
+//     let currentVal = Math.floor(Math.random() * max) + min;
+//     console.log(" Intercepted Function - Retrieved random number: ", currentVal);
+//
+//     //Record computer's plays.
+//     plays.push(currentVal);
+//     console.log(" Current list of computer plays", plays);
+//     console.log(" _______Computer Play Done_______");
+//
+//     //Return the random number that was generated
+//     return currentVal;
+// };
+
+
+/**
+ * Play back Phase
+ */
+
+var clickCounter = 0;
+var gameContainer = document.getElementById("gameContainer");
+
+// Performs the five Mouse Pointer Events that constitute a mouse click at a given location on the screen.
+doClickEvent = function(x, y) {
+
+    //Create Fake Mouse Pointer Events.
     var fakePointerOverEvent = document.createEvent("MouseEvent");
-    fakePointerOverEvent.initMouseEvent("pointerover", true, true, window, 0, 360, 410, 360, 410, false, false, false, false, 0, null);
+    fakePointerOverEvent.initMouseEvent("pointerover", true, true, window, 0, x, y, x, y, false, false, false, false, 0, null);
 
     var fakeMouseEvent = document.createEvent("MouseEvent");
-    fakeMouseEvent.initMouseEvent("click", true, true, window, 0, 360, 410, 360, 410, false, false, false, false, 0, null);
+    fakeMouseEvent.initMouseEvent("click", true, true, window, 0, x, y, x, y, false, false, false, false, 0, null);
 
     var fakePointerDownEvent = document.createEvent("MouseEvent");
-    fakePointerDownEvent.initMouseEvent("pointerdown", true, true, window, 0, 360, 410, 360, 410, false, false, false, false, 0, null);
+    fakePointerDownEvent.initMouseEvent("pointerdown", true, true, window, 0, x, y, x, y, false, false, false, false, 0, null);
 
     var fakePointerUpEvent = document.createEvent("MouseEvent");
-    fakePointerUpEvent.initMouseEvent("pointerup", true, true, window, 0, 360, 410, 360, 410, false, false, false, false, 0, null);
+    fakePointerUpEvent.initMouseEvent("pointerup", true, true, window, 0, x, y, x, y, false, false, false, false, 0, null);
 
     var fakePointerOutEvent = document.createEvent("MouseEvent");
-    fakePointerOutEvent.initMouseEvent("pointerout", true, true, window, 0, 360, 410, 360, 410, false, false, false, false, 0, null);
+    fakePointerOutEvent.initMouseEvent("pointerout", true, true, window, 0, x, y, x, y, false, false, false, false, 0, null);
 
+    //Dispatch the fake events.
     gameContainer.dispatchEvent(fakePointerOverEvent);
     gameContainer.dispatchEvent(fakePointerDownEvent);
     gameContainer.dispatchEvent(fakePointerUpEvent);
     gameContainer.dispatchEvent(fakePointerOutEvent);
     gameContainer.dispatchEvent(fakeMouseEvent);
-    clickCounter++;
+
+    //For initial double click
+    // clickCounter++;
 };
-// var gameContainer = document.getElementById("gameContainer");
-//
+
+let x = 360;
+let y = 410;
+
+//Perform a click every second on the document at a given location.
+setInterval(doClickEvent(x, y), 1000);
+
 shouldClick = function () {
     setInterval(() => {
         if (clickCounter < 2) {
-            doClickEvent();
+            doClickEvent(360, 410, 360, 410);
         }
     }, 200);
 
@@ -85,22 +120,30 @@ shouldClick = function () {
 // setTimeout(shouldClick, 100);
 setTimeout(shouldClick, 1000);
 
-var currentCount = -1;
-var plays = [];
 
-// let originalRandomIntGen = Math.floor(Math.random() * 4);
+//Monkey-Patched Phaser Random Integer Generator.
+var reproducedPlays = [0, 0, 0, 0, 0, 0]; //theoretically, this array could hold as many integers between 0 and 3 as
+// possible
+var indexRetrieved = 0;
+var compPlaysInPlaybackMode = [];
 Phaser.RandomDataGenerator.prototype.integerInRange = (min, max) => {
-    let currentVal = 0; //Math.floor(Math.random() * 4);
-    console.log("We intercepted their fucking function??? and retrieved: ", currentVal);
-    plays.push(currentVal);
-    console.log("current list of computer plays", plays);
 
+    //Gets the index number for the box from the list of existing data.
+    let currentVal = reproducedPlays[indexRetrieved];
+    compPlaysInPlaybackMode.push(currentVal);
+    console.log(" Intercepted Function - Retrieved Box 0 from the pre-recorded list of Computer Plays");
+    console.log(" Current list of computer plays", compPlaysInPlaybackMode );
+    indexRetrieved++;
+
+    console.log(" _______Computer Play Done_______");
+
+    //Return the retrieved index number
     return currentVal;
-    // currentCount++;
-    // plays.push(currentCount);
-    // return currentCount%4;
 };
-// setInterva
 
 
-setInterval(doClickEvent, 1000);
+
+
+
+
+
